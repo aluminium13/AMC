63aff88f896031243b5307033fd1a087bde285f8
Александра Кузьменко <aluminium_71@outlook.com>
2018-03-18 20:18:09 +0200

    trying to do smthng

--- labs/lab4.py
+++ labs/lab4.py
@@ -7,12 +7,15 @@ def eqation(x):
 def solve(f, eps):
     x = set()
     for x0 in range(-1000,1000,10):
-        lastX = x0
-        nextX = lastX + 10* eps  # "different than lastX so loop starts OK
-        while (abs(lastX - nextX) > eps):  # this is how you terminate the loop - note use of abs()
-            lastX = nextX
-            nextX = lastX - f(nextX)   / derivative(f, lastX, eps)  # update estimate using N-R
-        x.add(round(nextX, len(str(eps))-2))
+        try:
+            lastX = x0
+            nextX = lastX + 10* eps  # "different than lastX so loop starts OK
+            while (abs(lastX - nextX) > eps):  # this is how you terminate the loop - note use of abs()
+                lastX = nextX
+                nextX = lastX - f(nextX)   / derivative(f, lastX, eps)  # update estimate using N-R
+            x.add(round(nextX, len(str(eps))-2))
+        except ZeroDivisionError:
+            pass
     if len(x) != 0:
         return "Рішення: x = " + str(x)
     else:
